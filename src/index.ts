23b3f48dd5fbc56f966fc26b9f018c0098dbef68
Marco Emrich <marco.emrich@codecentric.de>
2021-06-13 04:08:23 +0200

    add Promise/fetch-Example

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-import { format } from "date-fns";
-const formattedDate: string = format(new Date(), "'Today is a ' eeee");
-console.log(formattedDate);
+import fetch from "isomorphic-fetch";
+
+const BASE_URL = "https://swapi.dev/api";
+
+const showPeople = async () => {
+  const people = await (await fetch(BASE_URL + "/people/")).json();
+  console.log(people.results.map((p: { name: string }) => p.name));
+};
+
+showPeople();
