773f69f4ee4717fe3aad9219a6d49aa1226c05a0
Marco Emrich <marco.emrich@codecentric.de>
2021-06-13 22:48:57 +0200

    remove old code

--- src/string_calc.spec.ts
+++ src/string_calc.spec.ts
@@ -1,7 +1,3 @@
-import { format } from "date-fns";
-const formattedDate: string = format(new Date(), "'Today is a ' eeee");
-console.log(formattedDate);
-
 test("add two numbers", () => {
   expect(1 + 2).toEqual(3);
 });
