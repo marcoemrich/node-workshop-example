a4ca72880b3c362e211e0f50429b94b5ed7da332
Marco Emrich <marco.emrich@codecentric.de>
2021-06-13 18:13:29 +0200

    complete Docker example

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
-import { format } from "date-fns";
-const formattedDate: string = format(new Date(), "'Today is a ' eeee");
-console.log(formattedDate);
+import * as http from "http";
+
+console.log("listening to localhost:1337 ...");
+
+http
+  .createServer((_req, res) => {
+    res.writeHead(200, {
+      "Content-Type": "text/plain",
+    });
+
+    res.write("Hello, World!\n");
+    console.log("Answering Client!");
+    res.end();
+  })
+  .listen(1337);
