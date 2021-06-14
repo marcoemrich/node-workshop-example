faf2ea9528966361674e277f2d46f8a1319c1c8e
Marco Emrich <marco.emrich@codecentric.de>
2021-06-14 17:23:26 +0200

    start to build pg example

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,25 @@
 import * as http from "http";
+import knex from "knex";
 
 console.log("listening to localhost:1337 ...");
+console.log("env", process.env);
+
+interface city {
+  id: number;
+  name: string;
+}
+
+const pg = knex({
+  client: "pg",
+  connection: process.env.PG_CONNECTION_STRING,
+  searchPath: ["knex", "public"],
+});
+
+const select = knex<City>("cities").select({
+  city: "a.city",
+});
+
+console.log("knex", select);
 
 http
   .createServer((_req, res) => {
